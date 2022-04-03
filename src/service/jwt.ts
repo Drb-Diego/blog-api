bd89917bde699f1ae7fcd0e8d152f6657e9bc3e8
Diego Brito <diegorbrito9@gmail.com>
2022-04-02 23:10:13 -0300

    new: criando função de validação do token

--- src/service/jwt.ts
+++ src/service/jwt.ts
@@ -3,13 +3,25 @@ import jwt from 'jsonwebtoken';
 
 const { SECRET } = process.env;
 
-const createToken = (token: Users) => {
+const createToken = (user: Users) => {
   if (SECRET) {
-    const tokenCreated = jwt.sign(token, SECRET);
+    const tokenCreated = jwt.sign(user, SECRET);
     return tokenCreated;
+  }
+};
+
+const validateToken = (token: string) => {
+  if (SECRET) {
+    try {
+      const decodedToken = jwt.verify(token, SECRET);
+      return decodedToken;
+    } catch (error) {
+      return null;
+    }
   }
 };
 
 export {
   createToken,
+  validateToken,
 };
